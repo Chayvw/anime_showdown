558101e8c5ccd1c2f43520e52b14132ba905aea9
Chay Williams <Chayvw@gmail.com>
2021-04-13 16:02:25 -0400

    added id to div tag to make sure onClick function works

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // connecting jQuery
 $(document).ready(function(){
-    console.log("jQuery is working")
+    // checking connection in the browser 
+    // console.log("jQuery is working")
+
+    // here we use jQuery to select the button via the ID with #getStarted
+    $("#getStarted").on("click", function(){
+        console.log("you've clicked the button")
+    })
+    
 })
