23f5255ead7af367a3475e1f09d9caf84696b121
Chay Williams <Chayvw@gmail.com>
2021-04-13 20:02:57 -0400

    added to card divs for fighters and 2 event listerns that display none to display block

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 // connecting jQuery
 $(document).ready(function(){
+    // const showFighters = document.getElementById("#selectFighter");
     // checking connection in the browser 
     // console.log("jQuery is working")
 
-    // here we use jQuery to select the button via the ID with #getStarted
+// functions that should run
+   function showFighter(){
+       $(".selection").attr("style" , "display:block")
+    // $("#selectFighter").attr("style", "display:block");
+
+   } 
+
+    // here we use jQuery to select the button via the ID with #getStarted with an on click event listener 
     $("#getStarted").on("click", function(){
-        console.log("you've clicked the button")
+        showFighter()
+        
+        // function showFighter(){
+        //   $("#selectFighter").style(display, "block");
+
+        // } document.getElementById("#selectFighter").style.display = block
+        
+        console.log("displayed worked")
+        // }
+        // showFighter()
     })
+
+    
+
+   
     
 })
