9d1a971bc1d1e5a3ddb2edef50e10e95848c9878
Chay Williams <Chayvw@gmail.com>
2021-04-19 17:47:43 -0400

    added goku attack feature

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ $(document).ready(function () {
     // checking connection in the browser 
     // console.log("jQuery is working")
 
-
     // where the objects for our fighters will be 
     const fighters = {
         goku: {
             name: "Goku",
             age: 37,
-            health:100,
+            health: 0,
             attacks: [{
                 name: "spirit bomb",
                 attkPoints: 90
@@ -24,38 +23,38 @@ $(document).ready(function () {
 
             {
                 name: "attack 3",
-                attkPoints: 40
+                attkPoints: 85
             }
 
             ],
             isAttacking: true,
-            defense:[{
-                name:"Solar Flare",
+            defense: [{
+                name: "Solar Flare",
                 defPoints: 80
             },
             {
-                name:"Power Stance",
+                name: "Power Stance",
                 defPoints: 45
             },
             {
-                name:"Def 3",
+                name: "Def 3",
                 defPoints: 22
             }
-        ],
+            ],
             // defense: ["Solar Flare", "Power Stance"],
             imgURL: "./images/goku.jpeg",
             gokusMove: function () {
-                if (this.isAttacking === true) {
-                    var userMove = Math.floor(Math.random() * fighters.goku.attacks.length)
-                    //    for(i = 0; i < fighters.goku.attacks.length; i++){
-                    //        console.log(i)
-                    //    }
-                    console.log(fighters.goku.attacks[userMove])
-                    // console.log(userMove)
-                }
-                else {
-                    console.log("a defensive move should be randomly picked")
-                }
+                // if (this.isAttacking === true) {
+                //     var userMove = Math.floor(Math.random() * fighters.goku.attacks.length)
+                //    for(i = 0; i < fighters.goku.attacks.length; i++){
+                //        console.log(i)
+                //    }
+                console.log(fighters.goku.attacks[userMove].attkPoints)
+                // console.log(userMove)
+                // }
+                // else {
+                //     console.log("a defensive move should be randomly picked")
+                // }
                 // i would like to add an on click event listener for when goku is selected it will render is image and have him attack or defend 
 
 
@@ -65,54 +64,54 @@ $(document).ready(function () {
         naurto: {
             name: "Naurto",
             age: 17,
-            health:100, 
-            attacks:[{
+            health: 100,
+            attacks: [{
 
-                name:"attack 4",
+                name: "attack 4",
                 attkPoints: 45
             },
 
             {
-                name:"attack 5",
+                name: "attack 5",
                 attkPoints: 88
             },
 
             {
-                name:"attack 6",
+                name: "attack 6",
                 attkPoints: 100
             }
 
-        ],
+            ],
             // attacks: ["Rasengan", "Tailed Beast Rasenshuriken"],
             isAttacking: true,
-            defense:[{
-                name:"defense",
+            defense: [{
+                name: "defense",
                 defPoints: 45
             },
             {
-                name:"55",
+                name: "def 2",
                 defPoints: 60
             },
             {
-                name:"Def 3",
+                name: "Def 3",
                 defPoints: 78
             }
-        ],
+            ],
             isDefending: false,
             imgURL: "./images/naruto.jpeg",
-            naurtosMove: function () {
-                if (this.isAttacking === true) {
-                    var randomNaurtoAttack = Math.floor(Math.random() * fighters.naurto.attacks.length)
-                    console.log(randomNaurtoAttack)
-                    console.log(fighters.naurto.attacks[randomNaurtoAttack]);
-                    
-                }
-                else {
-                    console.log()
-                }
+            // naurtosMove: function () {
+            //     if (this.isAttacking === true) {
+            //         var randomNaurtoAttack = Math.floor(Math.random() * fighters.naurto.attacks.length)
+            //         console.log(randomNaurtoAttack)
+            //         console.log(fighters.naurto.attacks[randomNaurtoAttack]);
+
+            //     }
+            //     else {
+            //         console.log()
+            //     }
 
 
-            }
+            // }
 
         },
         // izukuMidoriya: {
@@ -136,10 +135,47 @@ $(document).ready(function () {
         // }
 
 
-
     }
-    fighters.goku.gokusMove()
-    fighters.naurto.naurtosMove()
+    function attackCallOne() {
+        let indexOne = Math.floor(Math.random() * fighters.goku.attacks.length)
+        console.log(indexOne)
+        let randomAttack = fighters.goku.attacks[indexOne].attkPoints;
+        updatedHealth = fighters.naurto.health - randomAttack ;
+        console.log("Your attack power is" + " " + randomAttack)
+        console.log("Your opponents new health" + " " + updatedHealth)
+        console.log("You picked Goku")
+        chooseGoku()
+    }
+    // function attackCallTwo() {
+    //     let indexTwo = Math.floor(Math.random() * fighters.naurto.attacks.length)
+    //     console.log(indexTwo)
+    //     let randomAttackTwo = fighters.naurto.attacks[indexTwo].attkPoints;
+    //     updatedHealthTwo = fighters.goku.health - randomAttackTwo ;
+    //     console.log("Your attack power is" + " " + randomAttackTwo)
+    //     console.log("Your opponents new health" + " " + updatedHealthTwo)
+        
+    //     chooseNaurto()
+    // }
+
+    // function attackCallTwo(){
+    //     let randomAttackTwo = fighters.naurto.attacks[1].attkPoints;
+    //     updatedHealthTwo = randomAttackTwo - fighters.goku.health;
+
+
+    //     console.log(randomAttackTwo)
+    //     console.log(updatedHealthTwo)
+    // }
+    function defenseCallTwo() {
+        let randomDefense = fighters.naurto.defense[1];
+        // console.log(randomDefense)
+    }
+
+    // attackCallOne()
+    // attackCallTwo()
+    defenseCallTwo()
+
+    // fighters.goku.gokusMove()
+    // fighters.naurto.naurtosMove()
     // console.log(fighters.goku.gokusMove())
     // console.log()
     // console.log(fighters.goku.attacks[0].attkPoints)
@@ -153,18 +189,18 @@ $(document).ready(function () {
 
     function chooseGoku() {
         $("#naurto").attr("style", "display:none");
-        $("#deku").attr("style", "display:none");
+        // $("#deku").attr("style", "display:none");
     }
 
     function chooseNaurto() {
-        $("#goku", "#deku").attr("style", "display:none");
-        $("#deku").attr("style", "display:none");
+        $("#goku").attr("style", "display:none");
+        // $("#deku").attr("style", "display:none");
     }
 
-    function chooseDeku() {
-        $("#goku").attr("style", "display:none");
-        $("#naurto").attr("style", "display:none");
-    }
+    // function chooseDeku() {
+    //     $("#goku").attr("style", "display:none");
+    //     $("#naurto").attr("style", "display:none");
+    // }
     // here we use jQuery to select the button via the ID with #getStarted with an on click event listener 
     $("#getStarted").on("click", function () {
         showFighter()
@@ -178,28 +214,52 @@ $(document).ready(function () {
         // }
         // showFighter()
     })
-    $("#goku-btn").on("click", function () {
-        chooseGoku()
-        console.log("You picked Goku")
-    });
+    $("#goku-btn").on("click", attackCallOne)
+    // if (fighters.goku.health === 0) {
+    //     console.log("Your health has reached 0")
+    // } else {
 
-    $("#naurto-btn").on("click", function () {
-        chooseNaurto()
-        console.log("You picked Naurto")
-    });
 
-    $("#deku-btn").on("click", function () {
-        chooseDeku()
-        console.log("You picked Deku")
-    });
 
-    $(".attackOptions1").on("click", function () {
-        console.log("You picked an Attack")
-    })
+   
+     
+        // chooseGoku()
+        // attackCallOne()
+        // console.log("You picked Goku")
+    
 
-    $(".defenseOptions1").on("click", function () {
-        console.log("You picked a counter")
-    })
+$("#naurto-btn").on("click", attackCallTwo);
+    // function attackCallTwo() {
+    //     let randomAttackTwo = fighters.naurto.attacks[1].attkPoints;
+    //     updatedHealthTwo = randomAttackTwo - fighters.goku.health;
+
+
+    //     console.log(randomAttackTwo)
+    //     console.log(updatedHealthTwo)
+    // }
+   
+
+
+
+// if (fighters.goku.health >= 0) {
+//     attackCallOne()
+
+// } else {
+
+// }
+
+$("#deku-btn").on("click", function () {
+    chooseDeku()
+    console.log("You picked Deku")
+});
+
+$(".attackOptions1").on("click", function () {
+    console.log("You picked an Attack")
+})
+
+$(".defenseOptions1").on("click", function () {
+    console.log("You picked a counter")
+})
 
     // $(("#goku").on("click" , function(){
     //     chooseGoku()
